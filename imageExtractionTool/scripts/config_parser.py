48065eaaacc3039de771e86fea72a9119465381b
Bonsa11 <sambodza@hotmail.co.uk>
2022-06-30 16:18:25 +0100

    automated config_parser.py

--- imageExtractionTool/scripts/config_parser.py
+++ imageExtractionTool/scripts/config_parser.py
@@ -1,10 +1,12 @@
+import os
 from configparser import ConfigParser
 
 
 def get_config():
     config_object = ConfigParser()
     try:
-        config_object.read("../scripts/config.ini")
+        path = os.path.join(os.path.dirname(__file__), "config.ini")
+        config_object.read(path)
         test = config_object['PATHS']
     except Exception as e:
         config_object.read("./imageExtractionTool/scripts/config.ini")

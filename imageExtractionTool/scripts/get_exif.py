b96091aaa57862cb9295be2b2cd4d0e139c0229a
Bonsa11 <sambodza@hotmail.co.uk>
2022-08-19 09:18:47 +0100

    fixing commands

--- imageExtractionTool/scripts/get_exif.py
+++ imageExtractionTool/scripts/get_exif.py
@@ -5,7 +5,7 @@ def get_EXIF(logger, fldr_path, path, suffix=''):
     """scrape EXIf data from moved JPGs
     """
     try:
-        command = rf"exiftool {os.path.join(fldr_path, path)} > {os.path.join(fldr_path, 'logs', 'exif', f'exif_out{suffix}.txt')}"
+        command = rf"exiftool {os.path.join(fldr_path, path)} | tee {os.path.join(fldr_path, 'logs', 'exif', f'exif_out{suffix}.txt')}"
         os.system(command)
     except Exception as err:
         logger.error(f'unable to get EXIF data: {err}')
